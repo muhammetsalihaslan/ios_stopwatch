860eb359574057ac310eec04eb308f3ac6082fe8
salih <muhammetsalihaslan@gmail.com>
2022-06-27 16:55:28 +0300

    second

---
+++ app.js
@@ -0,0 +1,13 @@
+const screen = document.querySelector('.screen');
+const tour = document.querySelector('.tur');
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+const reset = document.querySelector('.reset');
+
+let msec = 0;
+let sec = 0;
+let min = 0;
+let stoptime = true;
+
+
+
