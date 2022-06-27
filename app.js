add5dcc086c91669511ff3f4ac2370f07883788c
salih <muhammetsalihaslan@gmail.com>
2022-06-27 18:03:36 +0300

    fourth

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const screen = document.querySelector('.screen');
-const tour = document.querySelector('.tur');
-const start = document.querySelector('.start');
-const stop = document.querySelector('.stop');
-const reset = document.querySelector('.reset');
+const laps = document.querySelector('.laps');
 
 let msec = 0;
 let sec = 0;
 let min = 0;
-let stoptime = true;
+let timer;
+
+function startTimer() {
+    if(!timer){
+        timer = setInterval(run, 10)
+     }
+    }
+
+function run() {
+    screen.textContent = (min < 10 ? '0' + min : min) + ":" + (sec < 10 ? '0' + sec : sec) + ":" + (msec < 10 ? '0' + msec : msec);
+    msec++;
+    if (msec == 100){
+        msec = 0;
+        sec++;
+    }
+    if(sec == 60){
+        sec = 0;
+        min++;
+    }
+    
+}
+
+
+
+
+
 
 
 
